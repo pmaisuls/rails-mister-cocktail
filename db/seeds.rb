7e72bd53ade0f93acba93b6b50e379386d5971df
Pablo Maisuls <pablo_maisuls@live.com>
2019-11-29 15:08:03 +0100

    push for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Scotch")
+Ingredient.create(name: "Coca-Cola")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Soda")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Dark rum")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Carbonated water")
+Ingredient.create(name: "Pineapple juice")
+Ingredient.create(name: "Ginger")
+
+puts 'Finished!'
+
+
+
+
+
+
+
+
+
